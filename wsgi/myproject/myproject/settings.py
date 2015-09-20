65ece96f909126a26fb7853fa68c94337b7b1eae
unknown <marjbuctolan@gmail.com>
2015-09-20 17:25:49 +0800

    first commit

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -33,7 +33,8 @@ DEBUG = os.environ.get('DEBUG') == 'True'
 from socket import gethostname
 ALLOWED_HOSTS = [
     gethostname(), # For internal OpenShift load balancer security purposes.
-    os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
+    os.environ.get('OPENSHIFT_APP_DNS'),
+    '*' # Dynamically map to the OpenShift gear name.
     #'example.com', # First DNS alias (set up in the app)
     #'www.example.com', # Second DNS alias (set up in the app)
 ]
@@ -48,6 +49,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hiworld',
 )
 
 MIDDLEWARE_CLASSES = (
